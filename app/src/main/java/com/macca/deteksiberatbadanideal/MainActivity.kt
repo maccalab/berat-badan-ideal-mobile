f8aa36540c513ec54b5512073776defb42ea6747
mrdyman <andimardimansaputra@gmail.com>
2022-08-06 22:59:59 +0800

    Initial Commit

---
+++ app/src/main/java/com/macca/deteksiberatbadanideal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.macca.deteksiberatbadanideal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
